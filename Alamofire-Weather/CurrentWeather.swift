cecd214eeb763571b2438a25b95a12827a27beda
Craig Scott <cscott_taakan@yahoo.com>
2018-09-19 13:28:21 -0700

    Added Current Weather model

---
+++ Alamofire-Weather/CurrentWeather.swift
@@ -0,0 +1,13 @@
+//
+//  CurrentWeather.swift
+//  Alamofire-Weather
+//
+//  Created by Craig Scott on 9/19/18.
+//  Copyright © 2018 Duc Tran. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWeather: UICollectionViewCell {
+    
+}
